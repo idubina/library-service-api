a7ef1e2ba183b588ffa92b335f34a9e81f78fe52
Illia Dubina <armahanz.music@gmail.com>
2026-07-11 17:52:31 +0300

    feat: "add borrowing serializers"
    - add Borrowing model serializer & model list serializer
    - add expected return date validation
    - add book avilability validation
    - add book inventory decrease after borrowing creation

--- library/serializers.py
+++ library/serializers.py
@@ -1,8 +1,51 @@
+from django.db import transaction
+from django.utils.timezone import now
 from rest_framework import serializers
-from library.models import Book
+from library.models import Book, Borrowing
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+            "is_active",
+        )
+        read_only_fields = ("id", "actual_return_date", "is_active", "user")
+
+    def validate(self, attrs):
+
+        if attrs["expected_return_date"] < now().date():
+            raise serializers.ValidationError(
+                "Expected return date cannot be in the past."
+            )
+        if not attrs["book"].inventory > 0:
+            raise serializers.ValidationError(
+                "The selected book is currently unavailable."
+            )
+
+        return attrs
+
+    @transaction.atomic
+    def create(self, validated_data):
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save(update_fields=["inventory"])
+        return super().create(validated_data)
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    book = serializers.StringRelatedField(read_only=True)
+    user = serializers.SlugRelatedField(read_only=True, slug_field="email")
