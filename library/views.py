85a4912ea653d7655f23536fb670c722e9a55a3d
Illia Dubina <armahanz.music@gmail.com>
2026-07-11 18:00:39 +0300

    feat: add Borrowing API View
    - add Borrrowing API view with list, retrieve, create ability
    - optimize queryset with select related for list endpoint
    - restrict regular users to their own borrowings
    - allow staff users to view all borrowings.

--- library/views.py
+++ library/views.py
@@ -1,6 +1,10 @@
-from rest_framework import viewsets
-from library.serializers import BookSerializer
-from library.models import Book
+from rest_framework import viewsets, mixins
+from library.serializers import (
+    BookSerializer,
+    BorrowingSerializer,
+    BorrowingListSerializer,
+)
+from library.models import Book, Borrowing
 from library.permissions import IsAdminOrAuthenticatedOrReadOnly
 
 
@@ -8,3 +12,30 @@ class BookApiView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (IsAdminOrAuthenticatedOrReadOnly,)
+
+
+class BorrowingApiView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        return self.serializer_class
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        if self.action == "list":
+            queryset = queryset.select_related("user", "book")
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
