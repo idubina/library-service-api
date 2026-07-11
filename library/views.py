d08e12669cb92612fa481e8245bb4993ec93f9cd
Illia Dubina <armahanz.music@gmail.com>
2026-07-11 15:16:50 +0300

    feat: add permission to allow read only for authenticared users & restrict all to staff users

--- library/views.py
+++ library/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from library.serializers import BookSerializer
 from library.models import Book
+from library.permissions import IsAdminOrAuthenticatedOrReadOnly
 
 
 class BookApiView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrAuthenticatedOrReadOnly,)
