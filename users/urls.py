966ab6d107da4e928f7c72bcc05dad36dc84060d
Illia Dubina <armahanz.music@gmail.com>
2026-07-11 13:08:16 +0300

    feat: add user endpoints routes with jwt token obtain/refresh/verify

---
+++ users/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from users.views import CreateUserApiView, MangeUserApiView
+
+app_name = "users"
+
+urlpatterns = [
+    path("register/", CreateUserApiView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", MangeUserApiView.as_view(), name="manage"),
+]
