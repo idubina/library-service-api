d1eafe58be88d522b360bb7687e5addd1fb9c391
Illia Dubina <armahanz.music@gmail.com>
2026-07-12 16:18:41 +0300

    feat: add return book action with only staff permission

--- library/views.py
+++ library/views.py
@@ -1,10 +1,15 @@
-from rest_framework import viewsets, mixins
-from rest_framework.permissions import IsAuthenticated
+from django.db import transaction
+from django.utils.timezone import now
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
 from library.serializers import (
     BookSerializer,
     BorrowingSerializer,
     BorrowingListSerializer,
+    BorrowingBookReturnSerializer,
 )
 from library.models import Book, Borrowing
 from library.permissions import IsAdminOrAuthenticatedOrReadOnly
@@ -33,6 +38,8 @@ class BorrowingApiView(
     def get_serializer_class(self):
         if self.action == "list":
             return BorrowingListSerializer
+        if self.action == "return_book":
+            return BorrowingBookReturnSerializer
         return self.serializer_class
 
     def get_queryset(self):
@@ -55,6 +62,31 @@ class BorrowingApiView(
         if self.action == "list":
             queryset = queryset.select_related("user", "book")
         return queryset
+
+    @action(
+        detail=True,
+        methods=["post"],
+        url_path="return",
+        permission_classes=(IsAdminUser,),
+    )
+    def return_book(self, request, pk=None):
+        borrowing = self.get_object()
+        with transaction.atomic():
+            if borrowing.is_active:
+
+                borrowing.actual_return_date = now()
+                borrowing.save(update_fields=["actual_return_date"])
+
+                book = borrowing.book
+                book.inventory += 1
+                book.save(update_fields=["inventory"])
+
+                serializer = self.get_serializer(borrowing)
+                return Response(serializer.data, status.HTTP_200_OK)
+        return Response(
+            {"detail": "This book is not currently borrowed."},
+            status.HTTP_400_BAD_REQUEST,
+        )
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
